4a99da58838cb322ed15997e37179deb74e3f343
Anderson Vieira Martins <andersonvmartins@gmail.com>
2021-05-27 23:31:42 -0300

    carga inicial

---
+++ esnext/promise.js
@@ -0,0 +1,12 @@
+//teste
+function falarDepoisDe(segundos, frase) {
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=> {
+            resolve(frase) // apenas um parâmetro pode ser passado
+         }, segundos * 1000 )
+    } )
+}
+
+falarDepoisDe(3,'Que Legal!')
+    .then(frase => frase.concat('?!?'))
+    .then(outraFrase => console.log(outraFrase))

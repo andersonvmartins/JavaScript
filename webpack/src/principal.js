2de312800ace107c4fecdd7ba078f4006538a48c
Anderson Vieira Martins <andersonvmartins@gmail.com>
2021-06-30 23:34:35 -0300

    nova carga

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,6 +1,9 @@
-import Pessoa from "./pessoa"
+import Pessoa from "./pessoa" 
 import './modulos/moduloA'
-import './assets/css/estilo.css'
+//import './assets/css/estilo.css'
+import './assets' // para pegar o index.js como padrão
+// o arquivo index.js tem a referencia para os css e scss
+
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,26 +1,56 @@
+const modoDEV = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
-const miniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDEV ? 'development' : 'production' ,
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    devServer: {
+        contentBase: "./public",
+        port: 9000
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
-        new miniCssExtractPlugin({
+        new MiniCssExtractPlugin({
             filename: 'estilo.css'
         })
     ],
     module: {
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/, //regex
             use: [
-                miniCssExtractPlugin.loader,
+                MiniCssExtractPlugin.loader,
                 /* 'style-loader', // adiciona CSS a DOM injetando a tag <style> */
-                'css-loader' // interpreta @import, url()..
+                'css-loader', // interpreta @import, url()..
+                'sass-loader',
+                
+
             ]
+        },{
+            test: /\.(png|svg|jpg|gif)$/,
+            use: [ {
+                loader: 'file-loader',
+                options: {
+                    name: "[contenthash].[ext]",
+                    esModule: false
+                }
+                
+            }]
+
         }]
     }
 }
